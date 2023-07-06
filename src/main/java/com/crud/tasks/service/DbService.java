2716f82d66e6d734b60a86d0a5122c021b513c26
Kasia Gierasimczuk <k.gierasimczuk@gmail.com>
2023-07-06 15:26:55 +0200

    Zadanie 19.2 - correction

--- src/main/java/com/crud/tasks/service/DbService.java
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -17,8 +18,8 @@ public class DbService {
         return repository.findAll();
     }
 
-    public Task getTaskById(Task task){
-        return repository.findById();
+    public Optional<Task> getTaskById(Task task){
+        return repository.findById(task.getId());
     }
 
 }
