07df6dd5ce3e31f8f1e083dd494ffac46acdf474
Kasia Gierasimczuk <k.gierasimczuk@gmail.com>
2023-07-06 14:50:17 +0200

    Revert "Zadanie 19.2 - wyszukiwanie pojedynczego zadania"
    This reverts commit 26e6890608d749fdf47530eb4e302e5c42ac36e6.

--- src/main/java/com/crud/tasks/domain/Task.java
+++ src/main/java/com/crud/tasks/domain/Task.java
@@ -1,34 +1,14 @@
 package com.crud.tasks.domain;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
-@NoArgsConstructor
-@AllArgsConstructor
 @Getter
-@Entity(name = "tasks")
+@AllArgsConstructor
 public class Task {
 
-    @Id
-    @GeneratedValue
     private Long id;
-
-    @Column(name = "name")
     private String title;
-
-    @Column(name = "description")
     private String content;
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getId() {
-        return id;
-    }
 }
