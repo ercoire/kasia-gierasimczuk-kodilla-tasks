4b3ed0e4478566ddf597a171845b42604d13c81f
Kasia Gierasimczuk <k.gierasimczuk@gmail.com>
2023-09-03 00:07:31 +0200

    Zadanie 32.2

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -1,5 +1,6 @@
 package com.crud.tasks.service;
 
+import com.crud.tasks.config.AdminConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -8,6 +9,8 @@ import org.thymeleaf.context.Context;
 
 @Service
 public class MailCreatorService {
+    @Autowired
+    private AdminConfig adminConfig;
 
     @Autowired
     @Qualifier("templateEngine")
@@ -16,6 +19,11 @@ public class MailCreatorService {
     public String buildTrelloCardEmail(String message) {
         Context context = new Context();
         context.setVariable("message", message);
+        context.setVariable("tasks_url", "http://localhost:8888/crud");
+        context.setVariable("button", "Visit website");
+        context.setVariable("admin_name", adminConfig.getAdminName());
+        context.setVariable("goodbye", "Have a nice day!");
+        context.setVariable("company", adminConfig.getCompanyName());
         return templateEngine.process("mail/created-trello-card-mail", context);
     }
 
